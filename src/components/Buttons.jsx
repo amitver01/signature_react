4138ea21e082be2c90235baa8c6f1a32059ffd81
Amit Verma <amitver6969@gmail.com>
2024-03-29 01:15:33 +0530

    button

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Buttons() {
+  return (
+    <>
+<button className="rounded-xl bg-gray-100 px-5 py-3 text-base font-medium text-navy-700 transition duration-200 hover:bg-gray-200 active:bg-gray-300 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 dark:active:bg-white/30">
+  DOWNLOAD
+</button>
+    </>
+  )
+}
+
+export default Buttons
