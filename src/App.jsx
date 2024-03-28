9038033dcfbade5eec7b0d83936f1df21bead2a3
Amit Verma <amitver6969@gmail.com>
2024-03-29 00:51:42 +0530

    01 start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>HELLO I AM STARTING MY PROJECT JAI SHREE RAM </h2>
+      <h3>NAMASTE</h3>
+      <div><H1>sYSTEM</H1></div>
+    </>
+  )
+}
+
+export default App
